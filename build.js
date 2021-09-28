ea1fb2a1f3912efb5114320f4fe930f28b2665c9
Bart Simons <bart@bartsimons.me>
2021-09-28 21:20:57 +0200

    Include build procedure for deployment to production.

---
+++ build.js
@@ -0,0 +1,31 @@
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
+import copy from 'rollup-plugin-copy';
+
+const copyConfig = {
+    targets: [
+        { src: 'index.html', dest: 'build' },
+        { src: 'a.out.wasm', dest: 'build' }
+    ]
+};
+
+const config = {
+    input: ['worker.js', 'a.out.js', 'components.js'],
+    output: {
+        dir: 'build/',
+        format: 'es'
+    },
+    plugins: [
+        minifyHTML(),
+        copy(copyConfig),
+        resolve()
+    ],
+    preserveEntrySignatures: false
+};
+
+if (process.env.NODE_ENV !== 'development') {
+    config.plugins.push(terser());
+}
+
+export default config;
---
+++ components.js
@@ -0,0 +1,6 @@
+import '@spectrum-web-components/theme/sp-theme.js';
+import '@spectrum-web-components/theme/theme-lightest.js';
+import '@spectrum-web-components/theme/scale-large.js';
+import '@spectrum-web-components/slider/sp-slider.js';
+
+import '@vaadin/vaadin-upload';
